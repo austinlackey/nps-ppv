c95044fd280686ab76d6480a155e0546a9c8363a
Austin Lackey <austinblackey@gmail.com>
2024-02-02 12:50:19 -0700

    Added Setup

---
+++ Code/setup.R
@@ -0,0 +1,29 @@
+# List of packages to install
+packages_to_install <- c(
+  "tidyverse", "openxlsx", "tcltk", "readxl", 
+  "plotly", "scales", "stringr", "anytime", 
+  "patchwork", "shiny", "shinyjs", "shinythemes", 
+  "shinydashboard", "shinyWidgets", "formattable", 
+  "lubridate"
+)
+
+# Install packages
+install.packages(packages_to_install)
+
+# Load installed packages
+library(tidyverse)
+library(openxlsx)
+library(tcltk)
+library(readxl)
+library(plotly)
+library(scales)
+library(stringr)
+library(anytime)
+library(patchwork)
+library(shiny)
+library(shinyjs)
+library(shinythemes)
+library(shinydashboard)
+library(shinyWidgets)
+library(formattable)
+library(lubridate)
